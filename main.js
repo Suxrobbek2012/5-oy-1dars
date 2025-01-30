ddbea4d8e7734fa1ebb0eda3784708aa47df9e58
Suxrobbek2012 <suxrobbek714@gmail.com>
2025-01-30 11:35:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// console.log("salom javascript")
+// console.log(5);
+// const num1 = +prompt("1sonni kriting");
+// const num2 = +prompt("2sonni kriting");
+// console.log(`${num1} + ${num} =${num1} + ${num}`)
+var ism = prompt("ismingizni kriting");
+console.log(ism);
+var familya = prompt("familyangizni kconsole.log(ism);riting");
+console.log(familya);
+var yosh = +prompt("yoshingizni kriting");
+console.log(yosh);
